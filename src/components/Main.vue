2a091d398dddfa11aa8fbfec9d51310849792711
mezo <mazinabrahim200@gmail.com>
2020-05-26 07:52:26 -0600

    first commit

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,28 +2,34 @@
     <div class="bg-white">
 
        
-        <div class="h-64 w-58 bg-red-700" :style="{ 'background-image': 'url (' + image + ')' }"> <!-- start header -->
+        <div class="h-64 w-58 bg-red-700 md:h-32" :style="{ 'background-image': 'url (' + image + ')' }"> <!-- start header -->
             <div class="py-8">
-               <div class="flex flex-col items-center">
+               <div class="flex flex-col items-center md:flex-row md:justify-between md:px-8">
                   <h1 class="font-bold text-2xl uppercase text-white">mezo<span class="text-yellow-500">s</span>tore</h1>
-                  <a href="" class="text-white text-base font-semibold mt-2 hover:bg-red-500 px-2 rounded">HOME</a>
-                  <a href="" class="text-white text-base font-semibold mt-2 hover:bg-red-500 px-2 rounded">ABOUT</a>
-                  <a href="" class="text-white text-base font-semibold mt-2 hover:bg-red-500 px-2 rounded">SHOPPING</a>
-                  <a href="" class="text-white text-base font-semibold mt-2 hover:bg-red-500 px-2 rounded">BLOG</a>
-                  <a href="" class="text-white text-base font-semibold mt-2 hover:bg-red-500 px-2 rounded">CONTACT</a>
+                   <div class="md:mr-4">
+                     <a href="" class="text-white text-base font-semibold mt-2 hover:bg-red-500 px-2 md:py-2 rounded text-center block md:inline">HOME</a>
+                  <a href="" class="text-white text-base font-semibold mt-2 hover:bg-red-500 px-2 md:py-2  rounded text-center block md:inline md:mx-2">ABOUT</a>
+                  <a href="" class="text-white text-base font-semibold mt-2 hover:bg-red-500 px-2 md:py-2  rounded text-center block md:inline md:mx-2">SHOPPING</a>
+                  <a href="" class="text-white text-base font-semibold mt-2 hover:bg-red-500 px-2 md:py-2  rounded text-center block md:inline md:mx-2">BLOG</a>
+                  <a href="" class="text-white text-base font-semibold mt-2 hover:bg-red-500 px-2 md:py-2  rounded text-center block md:inline md:mx-2">CONTACT</a>
+                   </div>
+
                </div>
             </div>
         </div>   <!-- end header -->
+          
 
-         <div class="py-12"> <!-- start content -->
+          <div class="md:flex md:flex-row md:py-8">   <!-- start md:screen content -->
+               
+         <div class="py-12 md:w-3/4"> <!-- start content -->
 
-            <h1 class="text-yellow-500 font-bold text-2xl uppercase ml-4"> All Products</h1>
+            <h1 class="text-yellow-500 font-bold text-2xl uppercase ml-4 md:px-32"> All Products</h1>
 
             <div class="mt-8">
-                <div class="grid grid-cols-1 px-4">
+                <div class="grid grid-cols-1 px-4 md:grid-cols-3 md:gap-4 md:px-32">
                   
-                       <div class="relative hover-trigger border pb-4 mt-8 w-full">
-                       <img class="h-56 w-full rounded object-cover" src="../assets/image/header.jpg">
+                       <div class="relative hover-trigger border pb-4 mt-8 w-full md:w-48">
+                       <img class="h-56 w-full md:w-48 rounded object-cover" src="../assets/image/header.jpg">
 
                           <button class="relative py-2  mx-auto px-8 rounded  -mt-12  bg-red-800 text-white hover:bg-red-500 font-semibold hover-target">
                             Add to Cart
@@ -38,8 +44,8 @@
 
                    </div>
 
-                    <div class="relative hover-trigger border pb-4 mt-8 w-full">
-                     <img class="h-56 w-full rounded object-cover" src="../assets/image/header.jpg">
+                    <div class="relative hover-trigger border pb-4 mt-8 w-full md:w-48">
+                     <img class="h-56 w-full md:w-48 rounded object-cover" src="../assets/image/header.jpg">
                        
                       <button class="relative py-2  mx-auto px-8 rounded  -mt-12  bg-red-800 text-white hover:bg-red-500 font-semibold hover-target">
                             Add to Cart
@@ -52,8 +58,8 @@
                        </div>
                    </div>
 
-                   <div class="relative hover-trigger border pb-4 mt-8 w-full">
-                       <img class="h-56 w-full rounded object-cover" src="../assets/image/header.jpg">
+                   <div class="relative hover-trigger border pb-4 mt-8 w-full md:w-48">
+                       <img class="h-56 w-full md:w-48 rounded object-cover" src="../assets/image/header.jpg">
 
                           <button class="relative py-2  mx-auto px-8 rounded  -mt-12  bg-red-800 text-white hover:bg-red-500 font-semibold hover-target">
                             Add to Cart
@@ -68,22 +74,9 @@
 
                    </div>
 
-                    <div class="relative hover-trigger border pb-4 mt-8 w-full">
-                     <img class="h-56 w-full rounded object-cover" src="../assets/image/header.jpg">
-                       
-                      <button class="relative py-2  mx-auto px-8 rounded  -mt-12  bg-red-800 text-white hover:bg-red-500 font-semibold hover-target">
-                            Add to Cart
-                          </button>
 
-                           <div>
-                          <h1 class="text-sm text-gray-900 mt-4 text-center font-bold mx-4 ">Women Maroon</h1>
-
-                        <h1 class="text-sm text-gray-700 mt-2 text-center font-bold mx-4"><span class="text-red-600 line-through mr-2">$975.00</span>$899.99</h1>
-                       </div>
-                   </div>
-
-                   <div class="relative hover-trigger border pb-4 mt-8 w-full">
-                       <img class="h-56 w-full rounded object-cover" src="../assets/image/header.jpg">
+                   <div class="relative hover-trigger border pb-4 mt-8 w-full md:w-48">
+                       <img class="h-56 w-full md:w-48 rounded object-cover" src="../assets/image/header.jpg">
 
                           <button class="relative py-2  mx-auto px-8 rounded  -mt-12  bg-red-800 text-white hover:bg-red-500 font-semibold hover-target">
                             Add to Cart
@@ -93,25 +86,48 @@
                        <div>
                           <h1 class="text-sm text-gray-900 mt-4 text-center font-bold mx-4 ">Women Maroon</h1>
 
-                        <h1 class="text-sm text-gray-700 mt-2 text-center font-bold mx-4"><span class="text-red-600 line-through mr-2">$975.00</span>$899.99</h1>
+                        <h1 class="text-sm text-gray-900 mt-2 text-center font-bold mx-4"><span class="text-teal-600 line-through mr-2">$975.00</span>$899.99</h1>
                        </div>
 
                    </div>
 
-                    <div class="relative hover-trigger border pb-4 mt-8 w-full">
-                     <img class="h-56 w-full rounded object-cover" src="../assets/image/header.jpg">
-                       
-                      <button class="relative py-2  mx-auto px-8 rounded  -mt-12  bg-red-800 text-white hover:bg-red-500 font-semibold hover-target">
+
+
+                   <div class="relative hover-trigger border pb-4 mt-8 w-full md:w-48">
+                       <img class="h-56 w-full md:w-48 rounded object-cover" src="../assets/image/header.jpg">
+
+                          <button class="relative py-2  mx-auto px-8 rounded  -mt-12  bg-red-800 text-white hover:bg-red-500 font-semibold hover-target">
                             Add to Cart
                           </button>
-
-                           <div>
+                   
+                        
+                       <div>
                           <h1 class="text-sm text-gray-900 mt-4 text-center font-bold mx-4 ">Women Maroon</h1>
 
-                        <h1 class="text-sm text-gray-700 mt-2 text-center font-bold mx-4"><span class="text-red-600 line-through mr-2">$975.00</span>$899.99</h1>
+                        <h1 class="text-sm text-gray-900 mt-2 text-center font-bold mx-4"><span class="text-teal-600 line-through mr-2">$975.00</span>$899.99</h1>
                        </div>
+
                    </div>
 
+
+
+                   <div class="relative hover-trigger border pb-4 mt-8 w-full md:w-48">
+                       <img class="h-56 w-full md:w-48 rounded object-cover" src="../assets/image/header.jpg">
+
+                          <button class="relative py-2  mx-auto px-8 rounded  -mt-12  bg-red-800 text-white hover:bg-red-500 font-semibold hover-target">
+                            Add to Cart
+                          </button>
+                   
+                        
+                       <div>
+                          <h1 class="text-sm text-gray-900 mt-4 text-center font-bold mx-4 ">Women Maroon</h1>
+
+                        <h1 class="text-sm text-gray-900 mt-2 text-center font-bold mx-4"><span class="text-teal-600 line-through mr-2">$975.00</span>$899.99</h1>
+                       </div>
+
+                   </div>
+
+                    
 
 
 
@@ -122,45 +138,46 @@
             
          </div> <!-- end content -->
 
-         <div class="px-4">
+         <div class="px-4 md:-px-16 md:w-1/4 md:mt-8">
             <div class="bg-gray-200 rounded-lg">
                  <div class="px-4 py-6">
                    <h1 class="text-2xl text font-semibold font-serif">MY<span class="text-yellow-700"> CART</span></h1>
                  </div>
 
-                  <div class="px-4">
-                    <div class="bg-red-300 rounded shadow-md">
-                       <div class="flex flex-row justify-between mt-4 px-4 bg-red-700 py-4 rounded-t shadow-md">
-                         <h1 class="text-white font-semibold">Checkout</h1>
-                         <h1 class="text-white font-semibold">Items</h1>
+                  <div class="px-4 pb-8">
+                    <div class="bg-gray-700 rounded shadow-md">
+                       <div class="flex flex-row justify-between mt-4 px-4 bg-gray-800 py-4 rounded-t shadow-md">
+                         <h1 class="text-white font-semibold ">Checkout</h1>
+                         <h1 class="text-white font-semibold ">Items</h1>
                        </div>
 
                        <div class="py-4 px-2 flex flex-row justify-between items-center border">
                          <img class="h-16 w-16 rounded" src="../assets/image/header.jpg">
-                         <h1 class="text-white font-semibold">Women Maroon</h1>
-                         <h1 class="text-white font-semibold">$899.99</h1>
+                         <h1 class="text-white font-semibold ">Women Maroon</h1>
+                         <h1 class="text-white font-semibold ">$899.99</h1>
                        </div>
 
                        <div class="py-4 px-2 flex flex-row justify-between items-center border">
                          <img class="h-16 w-16 rounded" src="../assets/image/header.jpg">
-                         <h1 class="text-white font-semibold">Women Maroon</h1>
-                         <h1 class="text-white font-semibold">$899.99</h1>
+                         <h1 class="text-white font-semibold ">Women Maroon</h1>
+                         <h1 class="text-white font-semibold ">$899.99</h1>
                        </div>
 
                        <div class="py-4 px-2 flex flex-row justify-between items-center border">
                          <img class="h-16 w-16 rounded" src="../assets/image/header.jpg">
-                         <h1 class="text-white font-semibold">Women Maroon</h1>
-                         <h1 class="text-white font-semibold">$899.99</h1>
+                         <h1 class="text-white font-semibold ">Women Maroon</h1>
+                         <h1 class="text-white font-semibold ">$899.99</h1>
                        </div>
 
                        <div class="py-4 px-2 flex flex-row justify-between items-center border">
-                         <h1 class="text-white font-semibold">Total $2400.90</h1>
-                         <button class="py-2 px-8 bg-red-700 text-white font-semibold hover:bg-red-500 rounded">Pay</button>
+                         <h1 class="text-white font-semibold ">Total $2400.90</h1>
+                         <button class="py-2 px-8 bg-white text-white text-gray-900 font-semibold hover:bg-gray-800 rounded">Pay</button>
                        </div>
                   </div>
                   </div>
             </div>
          </div>
+          </div>
         
     </div>
 </template>
