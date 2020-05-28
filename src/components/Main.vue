81e59dd6b79fd5bc0388d20756e33accef98cbc2
mezo <mazinabrahim200@gmail.com>
2020-05-28 14:05:27 -0600

    28-5

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -29,7 +29,7 @@
       <div class="grid grid-cols-1 px-4 md:grid-cols-3 md:gap-4 md:px-32">
 
        <div class="relative hover-trigger border pb-4 mt-8 w-full md:w-48" v-for="(product,index) in products">
-         
+
          <img class="h-56 w-full md:w-48 rounded object-cover"  :src="product.image">
 
          <button @click="toCart(product)" class="relative py-2  mx-auto px-8 rounded  -mt-12  bg-red-800 text-white hover:bg-red-500 font-semibold hover-target">
@@ -47,11 +47,11 @@
 
 
 
-     
 
 
-</div>
-</div>
+
+    </div>
+  </div>
 
 </div> <!-- end content -->
 
@@ -62,10 +62,10 @@
    </div>
 
    <div class="px-2 pb-8">
-    <div class="bg-gray-700 rounded shadow-md">
-     <div class="flex flex-row justify-between mt-4 px-4 bg-gray-800 py-4 rounded-t shadow-md">
-       <h1 class="text-white font-semibold ">Checkout</h1>
-       <h1 class="text-white font-semibold ">Items</h1>
+    <div class="bg-white rounded shadow-md">
+     <div class="flex flex-row justify-between mt-4 px-4 bg-white py-4 rounded-t shadow-md">
+       <h1 class="text-gray-900 font-semibold ">Checkout</h1>
+       <h1 class="text-gray-900 font-semibold ">Items</h1>
 
 
      </div>
@@ -73,25 +73,25 @@
      <div class="py-4 px-2 flex flex-row justify-between items-center border" v-for="(cart,index) in carts">
        <div class="static ">
         <img class="h-16 w-16 rounded" :src="cart.image">
-        <h2 class="absolute  text-black font-semibold w-4 h-4 px-0 py-0 bg-white rounded-full text-xs -mt-16 ml-16">43</h2>
-       </div>
-       <h1 class="text-white font-semibold text-base md:text-sm">{{cart.name}}</h1>
-        <button class="text-white font-semibold text-2xl"><Plus/></button>
-        <h1 class="text-white font-semibold text-base md:text-sm">${{cart.price}}</h1>
-       <button class="text-white font-semibold text-2xl"><Minus/></button>
-     </div>
+        <h2 class="absolute  text-white font-semibold w-4 h-4 px-0 py-0 bg-gray-900 rounded-full text-xs -mt-16 mg mx-16 pl-1">{{count}}</h2>
+      </div>
+      <h1 class="text-gray-900 font-semibold text-base md:text-sm">{{cart.name}}</h1>
+      <button class="text-gray-900 font-semibold text-2xl" @click="Add()"><Plus/></button>
+      <h1 class="text-gray-900 font-semibold text-base md:text-sm">${{cart.price}}</h1>
+      <button class="text-gray-900 font-semibold text-2xl" @click="Min()"><Minus/></button>
+    </div>
 
-      
-     
 
-     
 
-     <div class="py-4 px-2 flex flex-row justify-between items-center border">
-       <button class="py-2 px-8 bg-white text-white text-gray-900 font-semibold hover:bg-gray-800 rounded">Pay</button>
-       <h1 class="text-white font-semibold ">Total </h1>
-     </div>
+
+
+
+    <div class="py-4 px-2 flex flex-row justify-between items-center border">
+     <button class="py-2 px-8 bg-gray-500 text-white text-gray-900 font-semibold hover:bg-gray-800 rounded">Pay</button>
+     <h1 class="text-gray-900 font-semibold ">Total ${{total}}</h1>
    </div>
  </div>
+</div>
 </div>
 </div>
 </div>
@@ -119,47 +119,81 @@ export default{
 
     return {
       products:[
-       {
+      {
         id:1,
         image:require('../assets/image/5.jpg'),
-        price:354,
+        price:5,
         name:'Women Maroon'
-       },
-       {
+      },
+      {
         id:2,
-         image:require('../assets/image/2.jpg'),
-        price:32,
+        image:require('../assets/image/2.jpg'),
+        price:4,
         name:'T-shirt'
-       },
+      },
 
-       {
+      {
         id:3,
-         image:require('../assets/image/4.jpg'),
-        price:45,
+        image:require('../assets/image/4.jpg'),
+        price:3,
         name:'Trousers'
-       },
+      },
+
       ],
       carts:[],
+      count:1,
+      total:0,
+      custemTotal:[]
 
-     
+
     }
 
-   
+
   },
 
 
 
-    methods:{
+  methods:{
 
-        toCart:function(product){
+    toCart(product){
 
-           this.carts.push(product);
+       this.carts.push(product);
 
-           this.carts = [...new Set(this.carts)]
+       this.carts = [...new Set(this.carts)]
+
           
-        }
-      },
+          
+          for(let check=0;check <= this.carts.length; check++){
+             
+             
+            this.custemTotal.push(this.carts[check].price)
+             
 
-}
-</script>
+             this.custemTotal = [...new Set(this.custemTotal)]
+
+             this.total = this.custemTotal.reduce((a,b) => a + b, 0)
+          
+          }
+
+
+
+          },
+
+          Add(){
+
+            this.count++;
+       
+          },
+
+          Min(){
+
+
+            if(this.count != 0){
+             this.count--;
+           }
+         },
+       },
+
+     }
+     </script>
 
